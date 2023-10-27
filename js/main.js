700e8810e55ac0ddedfa19bbb4d3a00aa759de8e
GIDEON KIPKEMOI YEBEI <145453820+Gideon-Yebei@users.noreply.github.com>
2023-10-27 18:45:01 +0300

    Modified

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const layers = document.querySelectorAll(".layer");
+
+// The column transition animation.
+function collapse() {
+  layers.forEach((layer) => {
+    if (layer.classList.contains("active")) {
+      layer.classList.remove("active");
+    }
+  });
+}
+
+function home() {
+  collapse();
+  layers[0].classList.add("active");
+}
+
+function about() {
+  collapse();
+  layers[1].classList.add("active");
+}
+
+function services() {
+  collapse();
+  layers[2].classList.add("active");
+}
+
+function portfolio() {
+  collapse();
+  layers[3].classList.add("active");
+}
+
+function blog() {
+  collapse();
+  layers[4].classList.add("active");
+}
+
+function contact() {
+  collapse();
+  layers[5].classList.add("active");
+}
