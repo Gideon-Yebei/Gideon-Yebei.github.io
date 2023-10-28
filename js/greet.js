8ff07e22cb29f631f1aca8a31528f87c9ad91c68
GIDEON KIPKEMOI YEBEI <145453820+Gideon-Yebei@users.noreply.github.com>
2023-10-28 19:39:55 +0300

    Modified

--- js/greet.js
+++ js/greet.js
@@ -2,8 +2,8 @@ const myDate = new Date();
 const hrs = myDate.getHours();
 let msg = "";
 
-if (hrs<12) msg = "&& Good Morning!";      // After 6am
-if (hrs >= 12 && hrs<17) msg = "&& Good Afternoon!";    // After 12pm
-if (hrs >= 17 && hrs<=24) msg = "&& Good Evening!";      // After 5pm
+if (hrs<12) msg = "&& GOOD MORNING!";      // After 6am
+if (hrs >= 12 && hrs<17) msg = "&& GOOD AFTERNOON!";    // After 12pm
+if (hrs >= 17 && hrs<=24) msg = "&& GOOD EVENING!";      // After 5pm
 
 document.write(msg);
--- js/typewriter.js
+++ js/typewriter.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     // check if text isn't finished yet
     if (i < (text.length)) {
       // add next character to h1
-      document.querySelector("h2").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+      document.getElementById("typewriter-text").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
 
       // wait for a while and call this function again for next character
       setTimeout(function () {
@@ -46,3 +46,61 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // start the text animation
   StartTextAnimation(0);
 });
+// document.addEventListener('DOMContentLoaded', function (event) {
+//   // array with texts to type in typewriter
+//   const dataText = [
+//     "Web Developer",
+//     "Python and Django Developer",
+//     "JAVA Developer",
+//     "Cyber Security",
+//     "Cloud Computing",
+//     "Open Source Contributor",
+//   ];
+//
+//   // Select the HTML element where you want to display the text
+//   const textElement = document.querySelector("h2");
+//
+//   // Initialize variables for tracking text and array index
+//   let textIndex = 0;
+//   let arrayIndex = 0;
+//
+//   function typeWriter() {
+//     // Check if the array element exists
+//     if (arrayIndex < dataText.length) {
+//       const text = dataText[arrayIndex];
+//
+//       // Check if the text isn't finished yet
+//       if (textIndex < text.length) {
+//         // Add the next character to the textElement
+//         textElement.innerHTML =
+//           text.substring(0, textIndex + 1) + '<span aria-hidden="true"></span>';
+//
+//         // Wait for a while and call this function again for the next character
+//         setTimeout(typeWriter, 150);
+//         textIndex++;
+//       } else {
+//         // Text finished, move to the next array element
+//         arrayIndex++;
+//         // Clear the textElement
+//         textElement.innerHTML = "";
+//
+//         // Call this function again after a timeout for the next text
+//         setTimeout(StartTextAnimation, 700);
+//       }
+//     }
+//   }
+//
+//   // Function to start the text animation
+//   function StartTextAnimation() {
+//     // Reset text and array index if we reach the end of the array
+//     if (arrayIndex >= dataText.length) {
+//       arrayIndex = 0;
+//     }
+//
+//     // Start typewriter animation
+//     typeWriter();
+//   }
+//
+//   // Start the text animation when the DOM content is loaded
+//   StartTextAnimation();
+// });
